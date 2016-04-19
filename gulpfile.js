685b99cd91c203cfdd313ac1e32d83ec589859ed
Ireneusz Luczak <luczak.irek@gmail.com>
2016-04-18 19:07:47 -0500

    have a working gulpfile that pushes all files to dist folder. Also serves content and watches for changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,122 @@
 // 1. LIBRARIES
 // - - - - - - - - - - - - - - -
-var less = require('gulp-less');
-var path = require('path');
-var gulp = require('gulp');
+var $            = require('gulp-load-plugins')();
+var gulp         = require('gulp');
+var argv         = require('yargs').argv;
+var browser      = require('browser-sync');
+var less         = require('gulp-less');
+var autoprefixer = require('gulp-autoprefixer');
+var sequence     = require('run-sequence');
+var rimraf       = require("rimraf");
 
-// 2. FILE PATHS
+// 2. VARIABLES
 // - - - - - - - - - - - - - - -
-var paths = {
+// Check for --production flag
+var isProduction = !!(argv.production);
+
+// Port to use for the development server.
+var PORT = 8000;
+
+// Browsers to target when prefixing CSS.
+var COMPATIBILITY = ['last 2 versions', 'ie >= 9'];
+
+// File paths to various assets are defined here.
+var PATHS = {
   assets: [
-    './client/**/*.*',
-    '!./client/templates/**/*.*',
-    '!./client/assets/{scss,js}/**/*.*'
+    'src/assets/**/*',
+    '!src/assets/{!img,js,less}/**/*'
   ],
   less: [
-    'node_modules/bootstrap/less/',
-    'node_modules/jasny-bootstrap/less/',
-    'src/less'
+    'bower_components/bootstrap/less',
+    'bower_components/jasny-bootstrap/less',
+    'src/less/**/*.less'
   ],
-  js: [
-
+  javascript: [
+    'bower_components/angular/angular.js',
+    'src/js/**/*.js'
   ]
 };
 
-gulp.task('default', function() {
-  // place code for your default task here
+//3. TASKS
+// - - - - - - - - - - - - - - -
+
+// Delete the "dist" folder
+// This happens every time a build starts
+gulp.task('clean', function(done) {
+  rimraf('dist', done);
 });
 
+// Copy files out of the assets folder
+// This task skips over the "img", "js", and "scss" folders, which are parsed separately
+gulp.task('copy', function() {
+  gulp.src(PATHS.assets)
+    .pipe(gulp.dest('dist/assets'));
+});
+
+// Copy page templates into finished HTML files
+gulp.task('pages', function() {
+  gulp.src('src/**/*.html')
+    .pipe(gulp.dest('dist'));
+});
+
+// Compiles Less
 gulp.task('less', function () {
   return gulp.src('src/less/app.less')
     .pipe(less({
-      paths: paths.less
+      paths: PATHS.less
     }))
+    .pipe(autoprefixer({
+			browsers: COMPATIBILITY,
+			cascade: false
+		}))
     .pipe(gulp.dest('dist/css/'));
+});
+
+// Combine JavaScript into one file
+// In production, the file is minified
+gulp.task('javascript', function() {
+  var uglify = $.if(isProduction, $.uglify()
+    .on('error', function (e) {
+      console.log(e);
+    })
+);
+
+  return gulp.src(PATHS.javascript)
+    .pipe($.sourcemaps.init())
+    .pipe($.concat('app.js'))
+    .pipe(uglify)
+    .pipe($.if(!isProduction, $.sourcemaps.write()))
+    .pipe(gulp.dest('dist/js'));
+});
+
+// Copy images to the "dist" folder
+// In production, the images are compressed
+gulp.task('images', function() {
+  var imagemin = $.if(isProduction, $.imagemin({
+    progressive: true
+  }));
+
+  return gulp.src('src/assets/img/**/*')
+    .pipe(imagemin)
+    .pipe(gulp.dest('dist/assets/img'));
+});
+
+// Start a server with LiveReload to preview the site in
+gulp.task('server', ['build'], function() {
+  browser.init({
+    server: 'dist', port: PORT
+  });
+});
+
+// Build the "dist" folder by running all of the above tasks
+gulp.task('build', function(done) {
+  sequence('clean', ['less', 'javascript', 'images', 'pages'], done);
+});
+
+// Build the  site, run the server, and watch for file changes
+gulp.task('default', ['build', 'server'], function() {
+  gulp.watch(PATHS.assets, ['copy', browser.reload]);
+  gulp.watch(['src/less/**/*.less'], ['less', browser.reload]);
+  gulp.watch(['src/js/**/*.js'], ['javascript', browser.reload]);
+  gulp.watch(['src/assets/img/**/*'], ['images', browser.reload]);
 });
